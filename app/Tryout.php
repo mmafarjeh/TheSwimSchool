a8341437aabf5ab0d4066a4e10b44988730af54b
Leping08 <dereks008@gmail.com>
2018-12-16 16:29:52 -0500

    Implement CalendarInterface, add php docs, fix Athletes method and add morphMany to PoolSessions for the Tryout model

--- app/Tryout.php
+++ app/Tryout.php
@@ -2,15 +2,32 @@
 
 namespace App;
 
+use App\Library\Interfaces\CalendarInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
 /**
+ *
+ * An Eloquent Model: 'Tryout'
+ *
  * A tryout is to see if you are skilled enough for the swim team
+ *
+ * @property integer $id
+ * @property integer $season_id
+ * @property integer $location_id
+ * @property integer $class_size
+ * @property \Illuminate\Support\Carbon $registration_open
+ * @property \Illuminate\Support\Carbon $event_time
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @property \Illuminate\Support\Carbon $deleted_at
+ * @property-read \App\Group $group
+ * @property-read \App\Location $location
+ * @property-read \App\Season $season
  */
 
-class Tryout extends Model
+class Tryout extends Model implements CalendarInterface
 {
 
     use SoftDeletes;
@@ -37,7 +54,7 @@ class Tryout extends Model
      */
     public function Athletes()
     {
-        return $this->hasMany(Athlete::class)->currentseason();
+        return $this->hasMany(Athlete::class);
     }
 
 
@@ -84,5 +101,20 @@ class Tryout extends Model
     public function scopeCurrentSeason($query)
     {
         return $query->where('s_t_season_id', STSeason::GetCurrentSeason()->id);
+    }
+
+    public function PoolSessions()
+    {
+        return $this->morphMany('App\PoolSession', 'model');
+    }
+
+    public function path()
+    {
+        return '/swim-team/tryouts/' . $this->id;
+    }
+
+    public function title()
+    {
+        return 'North River Swim Team Tryout';
     }
 }
